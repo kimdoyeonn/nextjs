d39ea86369c4f62d865d9aa9dc4c8022e92d4597
doyeon <doyeon311@gmail.com>
2022-10-05 23:47:48 +0900

    카카오 맵 키 발금 & 연결

--- tareungyi/pages/_app.tsx
+++ tareungyi/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Script
+      src="//dapi.kakao.com/v2/maps/sdk.js?appkey=61b5541618ba489c12127ce95ed343d8&libraries=services,clusterer&autoload=false"
+      strategy="beforeInteractive"
+    />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- tareungyi/pages/index.tsx
+++ tareungyi/pages/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useEffect } from 'react'
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
 import styles from '../styles/Home.module.css'
 
 interface IProps {
@@ -12,24 +12,31 @@ const Home: NextPage<IProps> = (props: IProps) => {
   const stationData = props.data.row
   console.log(stationData)
   return (
-    <div className={styles.container}>
-      <h1>따릉이 정류소 현황</h1>
-      <ul>
-        {stationData.map((station: any) => {
-          return <li key={station.stationId}>
-            { station.stationName }
-          </li>
-        })}
-        <li>
-
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className={styles.container}>
+        <h1>따릉이 정류소 현황</h1>
+        <ul>
+          {stationData.map((station: any) => {
+            return <li key={station.stationId}>
+              { station.stationName }
+            </li>
+          })}
+        </ul>
+        <Map
+          center={{ lat: 33.5563, lng: 126.79581 }}
+          style={{ width: "100%", height: '100%' }}
+        >
+          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+            <div style={{color:"#000"}}>Hello World!</div>
+          </MapMarker>
+        </Map>
+      </div>
+    </>
   )
 }
 
 Home.getInitialProps = async function () {
-  const res = await axios.get(`http://openapi.seoul.go.kr:8088/7476585154646f7938364c686c4d52/json/bikeList/1/5/`)
+  const res = await axios.get(`http://openapi.seoul.go.kr:8088/7476585154646f7938364c686c4d52/json/bikeList/1/100/`)
   return {
     data: res.data.rentBikeStatus,
   }
