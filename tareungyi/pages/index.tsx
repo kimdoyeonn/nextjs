81e6efc5296402d3fd9b4331ef27df15a17de530
doyeon <doyeon311@gmail.com>
2022-10-06 23:12:40 +0900

    Feat: 서울시 화장실 데이터 api 연결 및 데이터 타입 정리

--- tareungyi/pages/index.tsx
+++ tareungyi/pages/index.tsx
@@ -1,44 +1,48 @@
 import axios from 'axios'
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
-import { Map, MapMarker } from 'react-kakao-maps-sdk'
 import styles from '../styles/Home.module.css'
 
 interface IProps {
-  data: any,
+  data: IToilet[],
+  count: number,
+}
+
+interface IToilet {
+  POI_ID: string,
+  FNAME: string,
+  ANAME: string,
+  CNAME: string,
+  CENTER_X1: number,
+  CENTER_Y1: number,
+  X_WGS84: number,
+  Y_WGS84: number,
+  INSERTDATE: string,
+  UPDATEDATE: string,
 }
 
 const Home: NextPage<IProps> = (props: IProps) => {
-  const stationData = props.data.row
-  console.log(stationData)
+  const { data: toiletLoc } = props;
   return (
     <>
       <div className={styles.container}>
-        <h1>따릉이 정류소 현황</h1>
+        <h1>서울시 화장실 위치</h1>
         <ul>
-          {stationData.map((station: any) => {
-            return <li key={station.stationId}>
-              { station.stationName }
-            </li>
+          {toiletLoc.map((toilet: IToilet) => {
+            return <li>{toilet.FNAME}</li>
           })}
         </ul>
-        <Map
-          center={{ lat: 33.5563, lng: 126.79581 }}
-          style={{ width: "100%", height: '100%' }}
-        >
-          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-            <div style={{color:"#000"}}>Hello World!</div>
-          </MapMarker>
-        </Map>
       </div>
     </>
   )
 }
 
 Home.getInitialProps = async function () {
-  const res = await axios.get(`http://openapi.seoul.go.kr:8088/7476585154646f7938364c686c4d52/json/bikeList/1/100/`)
+  const res = await axios.get(`http://openapi.seoul.go.kr:8088/4a73667662646f793130395567645751/json/SearchPublicToiletPOIService/1/100/`)
+  console.log(res.data.SearchPublicToiletPOIService)
+
+  const { row: data, list_total_count: count } = res.data.SearchPublicToiletPOIService
   return {
-    data: res.data.rentBikeStatus,
+    data, count
   }
 }
 
